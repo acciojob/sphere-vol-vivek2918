d323d81fef33bea3aac5fcdea8a13ed646c83a82
Vivek Chovatiya <119297870+vivek2918@users.noreply.github.com>
2023-06-24 10:50:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ function volume_sphere() {
     // Calculate the volume of the sphere
     var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
 
+    // Round the volume to 4 decimal places
+    volume = volume.toFixed(4);
+
     // Display the volume in the volume field
     document.getElementById('volume').value = volume;
 
     // Return false to prevent the form from submitting
-    return false; 
+    return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
